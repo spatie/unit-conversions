cf23a4a1506f28efcb9373e5019c054d993385cb
Alex Manase <malexandru699@gmail.com>
2023-02-06 15:48:30 +0200

    refactor: WeightTest

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -5,13 +5,8 @@ namespace Spatie\UnitConversions\Tests;
 use PHPUnit\Framework\TestCase;
 use Spatie\UnitConversions\Weight;
 
-class WeightTest extends TestCase
-{
-    /** @test */
-    public function it_can_convert_kilograms_to_lbs()
-    {
-        $lbs = Weight::fromKilograms(100)->toLbs();
+it('can convert kilograms to lbs', function () {
+    $lbs = Weight::fromKilograms(100)->toLbs();
 
-        $this->assertEquals(220.4623, $lbs);
-    }
-}
+    expect(bccomp($lbs, 220.4623, 4))->toBe(0);
+});
