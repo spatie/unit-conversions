372912daaaccdb6ccd6107ad2017a12e6a14cae2
Alex Manase <malexandru699@gmail.com>
2023-02-06 15:39:24 +0200

    refactor: TemperatureTest

--- tests/TemperatureTest.php
+++ tests/TemperatureTest.php
@@ -1,25 +1,15 @@
 <?php
 
-namespace Spatie\UnitConversions\Tests;
-
-use PHPUnit\Framework\TestCase;
 use Spatie\UnitConversions\Temperature;
 
-class TemperatureTest extends TestCase
-{
-    /** @test */
-    public function it_can_convert_celsius_to_fahrenheit()
-    {
-        $fahrenheit = Temperature::fromCelsius(100)->toFahrenheit();
+it('can convert celsius to fahrenheit', function () {
+    $fahrenheit = Temperature::fromCelsius(100)->toFahrenheit();
 
-        $this->assertEquals(212, $fahrenheit);
-    }
+    expect($fahrenheit)->toEqual(212);
+});
 
-    /** @test */
-    public function it_can_convert_celsius_to_kelvin()
-    {
-        $kelvin = Temperature::fromCelsius(100)->toKelvin();
+it('can convert celsius to kelvin', function () {
+    $kelvin = Temperature::fromCelsius(100)->toKelvin();
 
-        $this->assertEquals(373.15, $kelvin);
-    }
-}
+    expect($kelvin)->toEqual(373.15);
+});
